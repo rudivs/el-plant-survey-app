f2de0e52a13ec83c0ecfe3d3b5e509e3665e0008
Rudi von Staden <rudi@forgood.co.za>
2020-08-23 13:38:05 +0200

    adds workbox serviceworker

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import merge from 'deepmerge';
 // use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from '@open-wc/building-rollup';
+import { generateSW } from 'rollup-plugin-workbox';
 
 // use createBasicConfig to do regular JS to JS bundling
 // import { createBasicConfig } from '@open-wc/building-rollup';
@@ -24,6 +25,13 @@ export default merge(baseConfig, {
   // if you use createSpaConfig, you can use your index.html as entrypoint,
   // any <script type="module"> inside will be bundled by rollup
   input: './index.html',
+  plugins: [
+    generateSW({
+      globDirectory: 'dist/',
+      globPatterns: ['**/*.{js,html}'],
+      swDest: 'dist/sw.js',
+    }),
+  ],
 
   // alternatively, you can use your JS as entrypoint for rollup and
   // optionally set a HTML template manually
